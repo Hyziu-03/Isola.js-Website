499a36635ee08920adc9c2a6f47e7d0476796a5a
Szymon Hyziak <szymon.hyziak@protonmail.com>
2023-03-25 19:20:56 +0100

    Improving A11y and adding dark theme

--- src/tools/index.js
+++ src/tools/index.js
@@ -2,3 +2,8 @@ export function downscale(size) {
 	return (size * 3) / 4;
 }
 export const screenWidth = window.innerWidth;
+
+export const isDark =
+	window.matchMedia("(prefers-color-scheme: dark)")
+		.matches === true;
+export const theme = isDark ? "dark" : "light";
